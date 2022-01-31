226009a1b951db4d13f97e14393c9cb05814270a
igorv <igorvolynets1985@gmail.com>
2022-02-01 00:42:57 +0300

    Create product structure2

---
+++ src/test/java/Calculator2Test.java
@@ -0,0 +1,16 @@
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.NoSuchElementException;
+
+public class Calculator2Test {
+
+
+
+
+}
